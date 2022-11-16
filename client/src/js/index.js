80ac74ae3fa0cb6fd3412efa11e3fb161179a1d4
Dane Cronin <danecronin@gmail.com>
2022-11-16 14:18:04 -0700

    added render functionality and enabled form element

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,12 +1,16 @@
 //Import Modules
 import  "./form";
-import "./submit";
 import {getDb, initdb, postDb} from './database';
+import { fetchCards } from './cards';
+import {toggleForm, clearForm} from './form';
 
 
 //Import Bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+  //Import CSS Files
+  import "../css/index.css";
 
 //Import Images
 import Logo from '../images/logo.png';
@@ -16,12 +20,44 @@ import Dog from '../images/dog.png';
 //Add images on load
 window.addEventListener('load', function () {
   initdb();
-  getDb();
-  postDb();
+  fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
   });
 
-  //Import CSS Files
-import "../css/index.css";
+  // Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+ })
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let email = document.getElementById("email").value;
+let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+if (submitBtnToUpdate == false) {
+  postDb(name, email, phone, profile);
+} else {
+
+  fetchCards();
+    // Toggles the submit button back to POST functionality
+  submitBtnToUpdate = false;
+}
+
+// Clear form
+clearForm();
+// Toggle form
+toggleForm();
+// Reload the DOM
+fetchCards();
+});
